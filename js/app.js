a106051f127edd8ab9017b8020f16a6926223143
evaristocuesta <evaristocg@gmail.com>
2020-08-07 19:13:07 +0200

    SEO optimization, email hidden and js refactoring

--- js/app.js
+++ js/app.js
@@ -2,29 +2,50 @@
 import FpsMeter from './fpsMeter.js';
 import FpsMeterDefault from './fpsMeterDefault.js';
 
-const card = document.querySelector('.card');
-const buttons = document.querySelectorAll('.card .btn');
-
-buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        card.classList.toggle('is-flipped');
-    });
-});
-
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-let fpsMeter;
-if (new URLSearchParams(document.location.search).get('showfps')) {
-    fpsMeter = new FpsMeterDefault();
-}
-else {
-    fpsMeter = new FpsMeter();
-}
-
-const anim = new Animation(canvas, context, fpsMeter);
 window.onresize = resize;
+window.onload = initialize;
+
+let anim;
 
 function resize() {
     anim.resize();
+}
+
+function initialize () {
+    email();
+    flip();
+    animation();
+}
+
+function email () {
+    const email = 'contact' + '@' + 'evaristocuesta' + '.' + 'com';
+    const emailElement = document.getElementById('email');
+    emailElement.children[1].innerHTML = email;
+    emailElement.setAttribute('href', 'mailto:' + email);
+}
+
+function flip () {
+    const card = document.querySelector('.card');
+    const buttons = document.querySelectorAll('.card .btn');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            card.classList.toggle('is-flipped');
+        });
+    });
+}
+
+function animation() {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    let fpsMeter;
+    if (new URLSearchParams(document.location.search).get('showfps')) {
+        fpsMeter = new FpsMeterDefault();
+    }
+    else {
+        fpsMeter = new FpsMeter();
+    }
+
+    anim = new Animation(canvas, context, fpsMeter);
 }
